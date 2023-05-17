3aa7dd050eb0281088e9b5af137aac8d359af8b3
Muadz Izh <muadzihharul@gmail.com>
2023-05-17 13:37:38 +0300

    initialize whatsappservice

--- app/Sevices/WhatsappService.php
+++ app/Sevices/WhatsappService.php
@@ -4,8 +4,23 @@ namespace App\Services;
 
 class WhatsappService
 {
+    protected $enabled;
+    protected $apikey;
+    protected $url;
+
+    public function __construct()
+    {
+        $this->enabled = config('whatsapp.enabled');
+        $this->apiKey = config('whatsapp.api_key');
+        $this->url = config('whatsapp.url');
+    }
+
     public function sendMessage($number, $message)
     {
+        if (!$this->enabled) {
+            return;
+        }
+        dd('hello');
         // Logika pengiriman pesan WhatsApp menggunakan API atau library pihak ketiga
         // ...
     }
