f7a8ce27fb8b4626485eb7cfbf7b2ee05411ba92
Muadz Izh <muadzihharul@gmail.com>
2023-05-20 11:56:53 +0300

    Fix : add cache when fetch wp post in home page

--- app/Http/Livewire/PenaPpibandirma.php
+++ app/Http/Livewire/PenaPpibandirma.php
@@ -4,30 +4,40 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
 
 class PenaPpibandirma extends Component
 {
     public $post, $posts;
 
-    public function fetchRecentPost(){
-        $client = new Client();
-
-        try {
-            $response = $client->request('GET', 'https://blog.ppibandirma.com/wp-json/wp/v2/posts?_embed');
-            $posts = json_decode($response->getBody(), true);
+    public function fetchRecentPosts()
+    {
+        $this->posts = Cache::remember('recent_posts_wp', now()->addMinutes(20), function(){
+            $client = new Client();
             
-            if (!empty($posts)) {
-                $this->post = $posts[0];
-                $this->posts = array_slice($posts, 1, 5);
+            try {
+                $response = $client->request('GET', 'https://blog.ppibandirma.com/wp-json/wp/v2/posts?_embed');
+                $posts = json_decode($response->getBody(), true);
+                
+                if (!empty($posts)) {
+                    return array_slice($posts, 0, 6);
+                }
+            } catch (\Exception $e) {
+                // Handle the exception, such as logging or displaying an error message
             }
             
-        } catch (\Exception $e) {
-            // Handle the exception, such as logging or displaying an error message
+            return [];
+        });
+        
+        // Assign the first post to $post
+        if (!empty($this->posts)) {
+            $this->post = $this->posts[0];
+            array_shift($this->posts); // Remove the first post from the posts array
         }
     }
 
     public function mount(){
-        $this->fetchRecentPost();
+        $this->fetchRecentPosts();
     }
     public function render()
     {
