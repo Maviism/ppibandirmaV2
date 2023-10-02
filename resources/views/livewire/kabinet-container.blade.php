52ad251b771e87a781ae4dcee110158145465204
Muadz Izh <muadzihharul@gmail.com>
2023-10-03 00:39:37 +0300

    Refactor : kabinet view

--- resources/views/livewire/kabinet-container.blade.php
+++ resources/views/livewire/kabinet-container.blade.php
@@ -3,13 +3,13 @@
         <div class="mt-10 text-2xl text-white">Kabinet {{ $kabinet->name }}</div>
         <div class="text-center text-gray-200">Periode {{$kabinet->periode}}</div>
     </div>
-    <div class="flex flex-wrap justify-center mb-12">
+    <div class="flex flex-wrap justify-center mb-12 ">
         @foreach($kabinet->kabinetPerson as $person)
-        <div class="relative bg-cover bg-center bg-no-repeat w-48 h-72" style="background-image: url({{ env('APP_URL') }}/storage/images/kabinet/{{$person->profile_pict_url}})">
-            <div class="absolute bottom-0 left-0 w-full h-18 flex flex-col text-center bg-white/75">
-                <div class="text-gray-900 text-md font-bold">{{$person->name}}</div>
-                <div class="text-gray-700 text-sm">{{$person->position}}</div>
-                <div class="text-black text-sm">{{$person->instagram}}</div>
+        <div class="relative w-48 m-1">
+            <img src="{{ env('APP_URL') }}/storage/images/kabinet/{{$person->profile_pict_url}}" alt="">
+            <div class="absolute bottom-0 left-0 w-full flex flex-col text-center bg-gradient-to-t from-black h-16">
+                <div class="text-gray-100 text-sm font-bold">{{$person->name}}</div>
+                <div class="text-gray-100 text-xs">{{$person->position}}</div>
             </div>
         </div>
         @endforeach
