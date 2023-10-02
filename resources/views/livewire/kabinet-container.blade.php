45303ad2c3b15a4383cd18604758aa7da7ed061a
Muadz Izh <muadzihharul@gmail.com>
2023-10-03 00:44:53 +0300

    Change width kabinet div

--- resources/views/livewire/kabinet-container.blade.php
+++ resources/views/livewire/kabinet-container.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="flex flex-wrap justify-center mb-12 ">
         @foreach($kabinet->kabinetPerson as $person)
-        <div class="relative w-48 m-1">
+        <div class="relative w-44 m-1">
             <img src="{{ env('APP_URL') }}/storage/images/kabinet/{{$person->profile_pict_url}}" alt="">
             <div class="absolute bottom-0 left-0 w-full flex flex-col text-center bg-gradient-to-t from-black h-16">
                 <div class="text-gray-100 text-sm font-bold">{{$person->name}}</div>
